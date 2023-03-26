2350a6430f984dcbea4d2f9a1aad1dda5cea8be1
Iamshakhawat <shakhawathossain.se@gmail.com>
2023-03-27 00:47:55 +0600

    added files to the tree

---
+++ Prac/Prac/Delegates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac
+{
+    public class Delegates
+    {
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public delegate int Sum(int num1, int num2);
+
+
+        public void Print(Sum _sum)
+        {
+            int x = _sum(number1, number2);
+            Console.WriteLine(x);
+        }
+
+        public void Print2(Func<int,int, int> som)
+        {
+            int x = som(number1, number2);
+            Console.WriteLine(x);
+        }
+
+
+    }
+}
---
+++ Prac/Prac/Program.cs
@@ -0,0 +1,19 @@
+﻿using Prac;
+
+Delegates x = new Delegates();
+
+x.number1 = 50;
+x.number2 = 100;
+x.Print(sum);
+
+x.Print(sum2);
+
+int sum(int x, int y)
+{
+    return x + y;
+}
+
+int sum2(int x, int y)
+{
+    return x * y;
+}
