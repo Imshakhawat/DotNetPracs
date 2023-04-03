4a1a7c6b715c24f58dc276a845f8b8e6ae1b0aa1
shakhawat1355 <hossain.shakhawat@brainstation-23.com>
2023-04-03 19:28:56 +0600

    added projections

---
+++ Prac/Linq/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Linq.DataSource;
+using Linq.LinqExamples;
+
+// How to store Data 
+//var x = Products.ProductList;
+
+//foreach (var item in x)
+//{
+//    Console.WriteLine(item);
+//}
+
+//var x1 = Customers.CustomerList;
+
+//foreach (var item in x1)
+//{
+//    Console.WriteLine(item);
+//}
+
+ProjectIons projectIons = new ProjectIons();
+
+projectIons.SelectProperty();
+
+
+
+
+
